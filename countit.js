4e9848740798340e55c93145a2993d9890d7128e
dbobb220 <david.bobb.220@gmail.com>
2019-05-08 20:50:15 -0500

    adding logic

--- countit.js
+++ countit.js
@@ -6,3 +6,29 @@ function countFunc() {
     let output = input
     document.getElementById("output").innerHTML = output;
 }
+
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+let inputValue = 'hello world';
+
+let countitArray = [];
+
+let countit = function(array) {
+  array = inputValue.split('');
+  let count = 0;
+  let key = '';
+  alphabet.forEach(function(val, idx, arr) {
+    for (let i = 0; i < array.length; i++) {
+      if (val === array[i]) {
+        count += 1;
+        key = val;
+      }
+    }
+    countitArray.push([key, count]); 
+    count = 0;
+    key = '';
+  });
+  return countitArray;
+}
+
+countit(inputValue);
