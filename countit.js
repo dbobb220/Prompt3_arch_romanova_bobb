8e7e796c67e38d5ec195ef17adf1acf7b33dcf40
masharomanova <mariavromanova@yahoo.com>
2019-05-08 20:27:17 -0500

    connected html to js with basic input/output functionality

---
+++ countit.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function countFunc() { 
+    let input = document.getElementById("input").value;
+    console.log(input);
+    let output = input
+    document.getElementById("output").innerHTML = output;
+}
