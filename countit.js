b8ee674032babc3c0d107aa14e425e62a426252a
dbobb220 <david.bobb.220@gmail.com>
2019-05-10 11:05:06 -0500

    add comments and reset result logic

--- countit.js
+++ countit.js
@@ -3,39 +3,53 @@
 let inputValue = "";
 const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 let countitArray = [];
+let returnString = '';
 
 function countFunc() { 
-    let inputValue = document.getElementById("input").value;
-    countit(inputValue);
-    document.querySelector("#output").innerHTML = returnValues(countitArray);
+  // clearing output DOM in case anything is in there
+  document.querySelector("#output").innerHTML = '';
+  returnString = '';
+  countitArray = [];
+  // variable for input text
+  let inputValue = document.getElementById("input").value;
+  // sending input through functions and adding it to DOM
+  countit(inputValue);
+  document.querySelector("#output").innerHTML = returnValues(countitArray);
 }
 
-
-
 let returnValues = (arr) => {
-    let returnString = '';
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i][0] !== '') {
-        returnString += (arr[i][0] + ":" + arr[i][1]) + " ";
-        }
-    }
-    return returnString;
+  // loop through results from countit to remove empty values
+  for (let i = 0; i < arr.length; i++) {
+      if (arr[i][0] !== '') {
+      returnString += (arr[i][0] + ":" + arr[i][1]) + " ";
+      }
+  }
+  return returnString;
 }
 
 let countit = function(array) {
+  // variables for input, count, and alphabet letter
   let arrayValue = array.split('');
   let count = 0;
   let key = '';
+  // loop through each letter of alphabet
   alphabet.forEach(function(val, idx, arr) {
+    // loop through each letter from input
     for (let i = 0; i < arrayValue.length; i++) {
+      // if letter in the alphabet matches input value
+      // use letter as key value and add one to the count
       if (val === arrayValue[i]) {
         count += 1;
         key = val;
       }
     }
+    // once loop through input is done for a letter, add it to an array
     countitArray.push([key, count]); 
+    // reset count and key to prepare it for the next letter in the alphabet
     count = 0;
     key = '';
   });
+  // return final array that has empty values 
+  // if letter isn't present and a key and count if letter is present
   return countitArray;
 }
